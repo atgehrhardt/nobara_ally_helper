98d51bd06d735d9563ffbece45c4395f755a493e
Andrew Tait Gehrhardt <atgehrhardt@gmail.com>
2023-12-11 16:32:19 -0500

    Added variables to make it easier to keep up to date

--- install.sh
+++ install.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
+
+# Specifies which kernel and rogue-enemy version to download
+KERNEL_FILE="kernel-6.6.4-205.fsync.ally.fc38.x86_64.tar.gz"
+KERNEL_URL="https://github.com/jlobue10/ALLY_Nobara_fixes/releases/download/v2.1.0/kernel-6.6.4-205.fsync.ally.fc38.x86_64.tar.gz"
+ROGUE_ENEMY_FILE="rogue-enemy-1.5.0-1.fc38.x86_64.rpm"
+ROGUE_ENEMY_URL="https://github.com/jlobue10/ALLY_Nobara_fixes/releases/download/v2.1.0/rogue-enemy-1.5.0-1.fc38.x86_64.rpm"
+
+# Obtain elevated priviledges
 sudo -v
 
 # Change to Downloads directory
 cd ~/Downloads
 
 # Download files using wget
-wget https://github.com/jlobue10/ALLY_Nobara_fixes/releases/download/v2.1.0/kernel-6.6.4-205.fsync.ally.fc38.x86_64.tar.gz --content-disposition
-wget https://github.com/jlobue10/ALLY_Nobara_fixes/releases/download/v2.1.0/rogue-enemy-1.5.0-1.fc38.x86_64.rpm --content-disposition
+wget $KERNEL_URL --content-disposition
+wget $ROGUE_ENEMY_URL --content-disposition
 
 # Extract tar.gz file
-tar xvf kernel-6.6.4-205.fsync.ally.fc38.x86_64.tar.gz
+tar xvf $KERNEL_FILE
 
 # Move rogue-enemy into RPM directory
-mv rogue-enemy-1.5.0-1.fc38.x86_64.rpm RPM/rogue-enemy-1.5.0-1.fc38.x86_64.rpm
+mv $ROGUE_ENEMY_FILE RPM/$ROGUE_ENEMY_FILE
 
 # Change into RPM directory and install RPMs
 cd RPM
@@ -23,6 +31,12 @@ curl -L https://github.com/corando98/steam-patch/raw/main/install.sh | sh
 
 # Install asusctl package
 sudo dnf install asusctl
+
+# Clean up file
+cd ~/Downloads
+rm -rf RPM
+rm $KERNEL_FILE
+rm $ROGUE_ENEMY_FILE
 
 # Reboot the system
 reboot
