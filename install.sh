8936de64e6f9dc3fb317e41cf4f7426fe355f049
Andrew Tait Gehrhardt <atgehrhardt@gmail.com>
2023-12-12 15:40:49 -0500

    updating kernel

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Specifies which kernel and rogue-enemy version to download
-KERNEL_FILE="kernel-6.6.4-206.fsync.ally.fc38.x86_64.tar.gz"
-KERNEL_URL="https://github.com/jlobue10/ALLY_Nobara_fixes/releases/download/v2.2.0/kernel-6.6.4-206.fsync.ally.fc38.x86_64.tar.gz"
+KERNEL_FILE="kernel-6.6.6-200.fsync.ally.fc38.x86_64.tar.gz"
+KERNEL_URL="https://github.com/jlobue10/ALLY_Nobara_fixes/releases/download/v2.2.0/kernel-6.6.6-200.fsync.ally.fc38.x86_64.tar.gz"
 ROGUE_ENEMY_FILE="rogue-enemy-1.5.1-1.fc38.x86_64.rpm"
 ROGUE_ENEMY_URL="https://github.com/jlobue10/ALLY_Nobara_fixes/releases/download/v2.2.0/rogue-enemy-1.5.1-1.fc38.x86_64.rpm"
 
@@ -19,7 +19,7 @@ wget $ROGUE_ENEMY_URL --content-disposition
 # Extract tar.gz file
 tar xvf $KERNEL_FILE
 
-# Move rogue-enemy into RPM directory
+# Install Rogue Enemy
 sudo dnf install ~/Downloads/$ROGUE_ENEMY_FILE
 
 # Change into RPM directory and install RPMs
