ce5026c90c9d65cf33f78d9161efe9df6962769e
Andrew Tait Gehrhardt <atgehrhardt@gmail.com>
2023-12-12 15:03:04 -0500

    Updated Kernel and Rogue Enemy versions

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Specifies which kernel and rogue-enemy version to download
-KERNEL_FILE="kernel-6.6.4-205.fsync.ally.fc38.x86_64.tar.gz"
-KERNEL_URL="https://github.com/jlobue10/ALLY_Nobara_fixes/releases/download/v2.1.0/kernel-6.6.4-205.fsync.ally.fc38.x86_64.tar.gz"
-ROGUE_ENEMY_FILE="rogue-enemy-1.5.0-1.fc38.x86_64.rpm"
-ROGUE_ENEMY_URL="https://github.com/jlobue10/ALLY_Nobara_fixes/releases/download/v2.1.0/rogue-enemy-1.5.0-1.fc38.x86_64.rpm"
+KERNEL_FILE="kernel-6.6.4-206.fsync.ally.fc38.x86_64.tar.gz"
+KERNEL_URL="https://github.com/jlobue10/ALLY_Nobara_fixes/releases/download/v2.2.0/kernel-6.6.4-206.fsync.ally.fc38.x86_64.tar.gz"
+ROGUE_ENEMY_FILE="rogue-enemy-1.5.1-1.fc38.x86_64.rpm"
+ROGUE_ENEMY_URL="https://github.com/jlobue10/ALLY_Nobara_fixes/releases/download/v2.2.0/rogue-enemy-1.5.1-1.fc38.x86_64.rpm"
 
 # Obtain elevated priviledges
 sudo -v
