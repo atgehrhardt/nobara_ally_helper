cec9f2dc6965b9e39bbd710a1125bef1c285c209
Andrew Tait Gehrhardt <atgehrhardt@gmail.com>
2023-12-27 16:37:08 -0500

    Fixing URLs

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Specifies which kernel and rogue-enemy version to download
-KERNEL_URL="https://github.com/jlobue10/ALLY_Nobara_fixes/releases/download/v2.2.0/kernel-6.6.7-205.fsync.ally.fc38.x86_64.tar.gz"
-ROGUE_ENEMY_URL="https://github.com/jlobue10/ALLY_Nobara_fixes/releases/download/v2.2.0/rogue-enemy-1.5.3-1.fc38.x86_64.rpm"
+KERNEL_URL="https://github.com/jlobue10/ALLY_Nobara_fixes/releases/download/v2.3.0/kernel-6.6.7-205.fsync.ally.fc38.x86_64.tar.gz"
+ROGUE_ENEMY_URL="https://github.com/jlobue10/ALLY_Nobara_fixes/releases/download/v2.3.0/rogue-enemy-1.5.3-1.fc38.x86_64.rpm"
 KERNEL_FILE="${KERNEL_URL##*/}"
 KERNEL_NAME="${KERNEL_FILE%.tar.gz}"
 ROGUE_ENEMY_FILE="${ROGUE_ENEMY_URL##*/}"
